c1adaaa36b1b97c442e735773ef8810152729cb2
dev14 <dev14@dev14-G41MT-S2>
2019-09-24 14:53:08 +0530

    initial commit by dhanusha

---
+++ SalesToolV2019/src/com/ust/beans/Main.java
@@ -0,0 +1,20 @@
+package com.ust.beans;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData objSalesData = new SalesData();
+		System.out.println("Welcome to sales App");
+		displayGreetings();
+		
+		objSalesData.display();
+
+	}
+
+	private static void displayGreetings() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
